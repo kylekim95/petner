5c28067f025b225c8f59208c7bc1d1f43b22e611
jjeongsu <jeongsu.dev@gmail.com>
2025-02-27 21:33:58 +0900

    chore : RecommendRoomCard 의 Mock Data 추가(#14)

--- src/components/travel/RecommendRoomCard.vue
+++ src/components/travel/RecommendRoomCard.vue
@@ -1,44 +1,49 @@
 <script setup lang="ts">
-import PNG_IMAGES from '../../../public/PNG-Image/image.ts';
+import MOCK_RECOMMENDED_ROOM from '@/constants/mock/recommendedRoom.ts';
 
-const mockupImageURL = PNG_IMAGES.recommendRoom;
+const rest = MOCK_RECOMMENDED_ROOM.roommaxcount - MOCK_RECOMMENDED_ROOM.roombasecount;
 </script>
 
 <template>
   <div class="d-flex gap-3 card-wrapper">
-    <img :src="mockupImageURL" alt="recommended room image" />
+    <img :src="MOCK_RECOMMENDED_ROOM.imageURL" alt="recommended room image" />
     <div class="room-info d-flex flex-column justify-content-between">
       <div class="d-flex flex-column gap-1">
-        <div class="room-info-title">별채 A</div>
+        <div class="room-info-title">{{ MOCK_RECOMMENDED_ROOM.roomtitle }}</div>
         <div>
           <span class="room-info-property"> 객실 인원 : </span>
           <span>
-            <i v-for="i of 4" :key="i" class="bi bi-person red"></i>
-            <i v-for="i of 3" :key="i" class="bi bi-person"></i>
+            <i
+              v-for="i of MOCK_RECOMMENDED_ROOM.roombasecount"
+              :key="i"
+              class="bi bi-person red"
+            ></i>
+            <i v-for="i of rest" :key="i" class="bi bi-person"></i>
           </span>
         </div>
         <div>
           <span class="room-info-property"> 객실 크기 : </span>
-          <span> 5평 (17m2)</span>
+          <span>
+            {{ MOCK_RECOMMENDED_ROOM.roomsize }}평 ({{ MOCK_RECOMMENDED_ROOM.roomsize2 }}m2)</span
+          >
         </div>
         <div>
           <span class="room-info-property"> 객실 수 : </span>
-          <span> 0 </span>
+          <span> {{ MOCK_RECOMMENDED_ROOM.roomcount }} </span>
         </div>
         <div class="room-info-notice text-gray-7">
-          ※ 위 정보는 2024년 10월에 작성된 정보로(정상요금), 해당 숙박시설 이용요금이 수시로
-          변동됨에 따라 이용요금 및 기타 자세한 사항은 홈페이지 참조 요망
+          {{ MOCK_RECOMMENDED_ROOM.roomintro }}
         </div>
         <div>
           <i v-for="i of 7" :key="i" class="bi bi-house"></i>
         </div>
       </div>
-      <div class="room-info-price">100000 / 박</div>
+      <div class="room-info-price">{{ MOCK_RECOMMENDED_ROOM.roomoffseasonminfee1 }} / 박</div>
     </div>
   </div>
 </template>
 
-<style>
+<style scoped>
 .card-wrapper {
   width: 1280px;
   height: 400px;
@@ -58,7 +63,7 @@ const mockupImageURL = PNG_IMAGES.recommendRoom;
 
 .room-info-title {
   font-size: 36px;
-  font-family: 'Paperlogy-8ExtraBold';
+  font-family: 'Paperlogy-7Bold';
 }
 
 .room-info-price {
