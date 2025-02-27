8985d12aabae118851a316ac200e6a1f863b300a
Joonsenia <128702580+Joon-Leo@users.noreply.github.com>
2025-02-27 16:29:58 +0900

    feat:Bootstrap color custom

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,4 +2,17 @@
 
 <template>
   <h1>HOME</h1>
+  <h4 class="bg-primary-green fs-1">primary-green</h4>
+  <h4 class="bg-secondary-green">secondary-green</h4>
+  <h4 class="bg-primary-blue">primary-blue</h4>
+  <h4 class="bg-secondary-blue">secondary-blue</h4>
+  <h4 class="bg-primary-red">primary-red</h4>
+  <h4 class="bg-primary-purple">primary-purple</h4>
+  <h4 class="bg-primary-red">secondary-red</h4>
+  <h4 class="bg-gray-1">gray-1</h4>
+  <h4 class="bg-gray-2">gray-2</h4>
+  <h4 class="bg-gray-3">gray-3</h4>
+  <h4 class="bg-gray-6">gray-6</h4>
+  <h4 class="bg-gray-7">gray-7</h4>
+  <h4 class="bg-gray-10">gray-10</h4>
 </template>
