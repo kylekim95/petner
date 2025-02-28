f2cf50d60322aad3800536be7a1c47173d367c00
cj2174 <cj2174@naver.com>
2025-02-28 11:36:14 +0900

    Cys Test save

--- src/views/test/CYSTestView.vue
+++ src/views/test/CYSTestView.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
 import AppFooter from '@/components/common/AppFooter.vue';
 import AppHeader from '@/components/common/AppHeader.vue';
-
 </script>
 
 <template>
   <div>
-    <AppHeader/>
-    <AppFooter/>
+    <AppHeader />
+    <AppFooter />
   </div>
-
 </template>
