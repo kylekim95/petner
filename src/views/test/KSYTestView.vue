ff8416d6961df288cdff5ab70e4683a73fd5b59a
SangYupKim <kylekim95@gmail.com>
2025-02-28 10:16:00 +0900

    feat #13 확장메뉴 (추후에 리팩토링 필요)

--- src/views/test/KSYTestView.vue
+++ src/views/test/KSYTestView.vue
@@ -1,7 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ExpandingMenu from '@/components/travel/ExpandingMenu.vue';
+import type { ExpandingMenuItemData } from '@/components/travel/ExpandingMenu.vue';
+
+const itemData : ExpandingMenuItemData[] = [
+  { id:0, bgImage:'http://tong.visitkorea.or.kr/cms/resource/82/3364482_image2_1.JPG', title:'title 1', content:'content 1' },
+  { id:1, bgImage:'http://tong.visitkorea.or.kr/cms/resource/82/3364482_image2_1.JPG', title:'title 2', content:'content 2' },
+  { id:2, bgImage:'http://tong.visitkorea.or.kr/cms/resource/82/3364482_image2_1.JPG', title:'title 3', content:'content 3' },
+];
+</script>
 
 <template>
   <div>
-    <h1>KSY Hello World!</h1>
+    <div style="height:400px; width:1000px">
+      <ExpandingMenu :item-data="itemData"/>
+    </div>
   </div>
 </template>
