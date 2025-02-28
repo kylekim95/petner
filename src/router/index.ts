3e5708485807937672ca93ef5be1b3147bc052a1
cj2174 <cj2174@naver.com>
2025-02-28 20:08:08 +0900

    Design : 유기동물 조회 상세페이지

--- src/router/index.ts
+++ src/router/index.ts
@@ -56,6 +56,11 @@ const router = createRouter({
       name: 'AdoptionSearch',
       component: () => import('../views/AdoptionSearch.vue'),
     },
+    {
+      path: '/AdoptionDetail',
+      name: 'AdoptionDetail',
+      component: () => import('../views/AdoptionDetail.vue'),
+    },
   ],
 });
 
--- src/views/AdoptionDetail.vue
+++ src/views/AdoptionDetail.vue
@@ -1,5 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppHeader from '@/components/common/AppHeader.vue';
+import AppFooter from '@/components/common/AppFooter.vue';
+</script>
 
 <template>
-  <div></div>
+  <div>
+    <div class="container-fluid p-0">
+      <AppHeader />
+
+      <div class="banner-container position-relative">
+        <div class="position-absolute top-50 start-0 translate-middle-y p-3 text-white">
+          <h1 class="display-4">유기동물 조회</h1>
+          <p class="lead">상세내용</p>
+        </div>
+        <img
+          src="/PNG-Image/images/adoptdetailbanner.png"
+          alt="Adopt Detail Banner"
+          class="img-fluid"
+          style="max-height: 384px"
+        />
+      </div>
+
+      <AppFooter />
+    </div>
+  </div>
 </template>
