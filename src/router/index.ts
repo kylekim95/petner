f18bb65b1a21a0fa8695608cd07dd1fa1d434b26
SangYupKim <kylekim95@gmail.com>
2025-03-04 08:54:28 +0900

    feat #50 플래너 페이지 기본 설정정

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,6 +8,7 @@ const router = createRouter({
     { path: PATH.root, name: 'home', component: ()=>import('@/views/HomeMain.vue'), meta: { headerVariant: 'header-transparent' } },
     { path: PATH.login, name: 'login', component: ()=>import('@/views/LoginView.vue'), meta: { headerVariant: 'header-default' } },
     { path: PATH.signup, name: 'signup', component: ()=>import('@/views/SignupView.vue'), meta: { headerVariant: 'header-default' } },
+    { path: PATH.travelPlanner, name: 'travelPlanner', component: ()=>import('@/views/TravelPlanner.vue'), meta: { headerVariant: 'header-red' } },
     ...ADOPTION_ROUTES
   ],
 });
