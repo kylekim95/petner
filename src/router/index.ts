46a640f969636e8bf577f7b7feecb9646bb310d2
SangYupKim <kylekim95@gmail.com>
2025-03-03 18:05:42 +0900

    feat #49 회원가입 & 로그인 페이지 퍼블리싱

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,8 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: PATH.root, name: 'home', component: ()=>import('@/views/HomeMain.vue'), meta: { headerVariant: 'header-transparent' } },
+    { path: PATH.login, name: 'login', component: ()=>import('@/views/LoginView.vue'), meta: { headerVariant: 'header-default' } },
+    { path: PATH.signup, name: 'signup', component: ()=>import('@/views/SignupView.vue'), meta: { headerVariant: 'header-default' } },
     ...ADOPTION_ROUTES
   ],
 });
