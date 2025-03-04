42eca67ced717b993e077d87c088bd707687531c
jjeongsu <jeongsu.dev@gmail.com>
2025-03-04 09:06:25 +0900

    Design : 보호소 조회 레이아웃 (#47)

--- src/components/adoption/shelter/ShelterSearchSection.vue
+++ src/components/adoption/shelter/ShelterSearchSection.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import TitleText from '@/components/common/TitleText.vue';
 import ShelterCard from './ShelterCard.vue';
 import ANIMAL_SHELTERS from '@/constants/mock/animalShelter';
 </script>
@@ -6,9 +7,12 @@ import ANIMAL_SHELTERS from '@/constants/mock/animalShelter';
 <template>
   <div class="search-wrapper d-flex">
     <div class="side-bar">
+      <TitleText size="24px" weight="600" color="gray-10">우리지역 보호소 찾기</TitleText>
+    </div>
+    <div class="side-bar">
       <ShelterCard v-for="(shelter, index) of ANIMAL_SHELTERS" :key="index" :shelter="shelter" />
     </div>
-    <div>map</div>
+    <div class="map">map</div>
   </div>
 </template>
 
@@ -16,9 +20,19 @@ import ANIMAL_SHELTERS from '@/constants/mock/animalShelter';
 .search-wrapper {
   width: 100vw;
   background-color: var(--gray-1);
+  height: 80vh;
+  max-height: 900px;
 }
 
 .side-bar {
-  width: 300px;
+  width: 15vw;
+  min-width: 200px;
+  background-color: red;
+  padding-top: 40px;
+}
+
+.map {
+  background-color: blue;
+  flex: 1;
 }
 </style>
