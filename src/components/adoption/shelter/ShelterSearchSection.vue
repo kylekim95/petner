c3a41d593fb6e65bee705aa1b9c632548d902287
jjeongsu <jeongsu.dev@gmail.com>
2025-03-04 10:54:31 +0900

    Design : 드롭다운 아이템 추가(#47)

--- src/components/adoption/shelter/ShelterSearchSection.vue
+++ src/components/adoption/shelter/ShelterSearchSection.vue
@@ -3,6 +3,10 @@ import TitleText from '@/components/common/TitleText.vue';
 import ShelterCard from './ShelterCard.vue';
 import ANIMAL_SHELTERS from '@/constants/mock/animalShelter';
 import { KOR_ORG, GU_ORG } from '@/constants/korOrg';
+import { ref } from 'vue';
+
+const upperOrg = ref('');
+const org = ref('하위지역을 선택해 주세요');
 </script>
 
 <template>
@@ -12,13 +16,36 @@ import { KOR_ORG, GU_ORG } from '@/constants/korOrg';
       <!-- ------------- -->
       <TitleText size="20px" weight="500" color="gray-10">시/도</TitleText>
       <div>
-        <button v-for="org of KOR_ORG" :key="org.orgCd" type="button" class="btn btn-light">
-          {{ org.orgdownNm.slice(0, 2) }}
+        <button
+          v-for="upperOrg of KOR_ORG"
+          :key="upperOrg.orgCd"
+          type="button"
+          class="btn btn-light"
+        >
+          {{ upperOrg.orgdownNm.slice(0, 2) }}
         </button>
       </div>
       <!-- ------------- -->
       <TitleText size="20px" weight="500" color="gray-10">시군구</TitleText>
-      <div></div>
+      <div>
+        <div class="btn-group">
+          <button
+            type="button"
+            class="btn dropdown-toggle"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            {{ org }}
+          </button>
+          <ul class="dropdown-menu">
+            <li v-for="orgItem of GU_ORG['6110000']" :key="org.orgCd">
+              <a class="dropdown-item" href="#" @click="org.value = orgItem.orgdownNm">{{
+                orgItem.orgdownNm
+              }}</a>
+            </li>
+          </ul>
+        </div>
+      </div>
       <!-- ------------- -->
       <TitleText size="20px" weight="500" color="gray-10">구조대상동물</TitleText>
     </div>
@@ -51,5 +78,18 @@ import { KOR_ORG, GU_ORG } from '@/constants/korOrg';
 .map {
   background-color: blue;
   flex: 1;
+}
+
+.dropdown-toggle {
+  border: 1px solid var(--gray-3);
+}
+
+.dropdown-toggle:hover {
+  background-color: var(--gray-3);
+  color: var(--gray-10);
+}
+
+.dropdown-item:hover {
+  background-color: var(--secondary-green);
 }
 </style>
