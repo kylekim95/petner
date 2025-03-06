f0080eda844763eca3ace3ba73b16f5f48100280
SangYupKim <kylekim95@gmail.com>
2025-03-07 00:07:25 +0900

    feat 메인화면 엔드포인트 연결결

--- src/components/homeMain/atoms/PopularTravelCard.vue
+++ src/components/homeMain/atoms/PopularTravelCard.vue
@@ -23,9 +23,9 @@ const props = defineProps<PopularTravelCardProps>();
         backgroundPositionX : 'center',
         backgroundSize : 'cover',
       }">
-      <p class="px-4 my-1 overflow-hidden location-text text-gray-1">{{ props.data.location }}</p>
+      <p class="px-4 my-3 overflow-hidden location-text text-gray-1">{{ props.data.location }}</p>
     </div>
-    <div class="pb-4 bg-gray-3 w-100 rounded-bottom-5" style="height: fit-content;">
+    <div class="pb-4 bg-gray-3 w-100 rounded-bottom-5">
       <p class="px-4 my-1 overflow-hidden title-text">{{ props.data.title }}</p>
       <p class="px-4 my-1 overflow-hidden content-text">{{ props.data.content }}</p>
     </div>
@@ -36,7 +36,7 @@ const props = defineProps<PopularTravelCardProps>();
 .location-text {
   font-family: 'Paperlogy';
   font-weight: 700;
-  font-size: 48px;
+  font-size: 20px;
 }
 .title-text {
   font-family: 'Paperlogy';
@@ -49,5 +49,8 @@ const props = defineProps<PopularTravelCardProps>();
   font-weight: 700;
   font-size: 20px;
   color: #BC8661;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 }
 </style>
