4560c29945ac42e4fe13aca0edf8dc5f3750c231
jjeongsu <jeongsu.dev@gmail.com>
2025-03-06 10:42:47 +0900

    chore: 동물 보소호 미리보기 목록 (#96)

--- src/apis/adoption/shelter.ts
+++ src/apis/adoption/shelter.ts
@@ -1,13 +1,13 @@
 import adoAPI from '@/config/axiosAdoConfig';
 import shelterAPI from '@/config/axiosShelterConfig';
 
-export const getShelterListApi = async () => {
+export const getShelterListApi = async (upr_cd: number, org_cd: number) => {
   try {
     const response = await adoAPI.get('/shelter', {
       params: {
         // 이후 수정
-        upr_cd: 6110000,
-        org_cd: 3110000,
+        upr_cd,
+        org_cd,
       },
     });
     console.log('res', response);
